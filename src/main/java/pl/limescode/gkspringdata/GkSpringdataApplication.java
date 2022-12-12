09da2049ce54c3c2808eb4520c20c9377b4af790
limescode <katechizis@gmail.com>
2022-12-12 20:58:00 +0100

    initial commit

---
+++ src/main/java/pl/limescode/gkspringdata/GkSpringdataApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkSpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkSpringdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkspringdata/GkSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkSpringdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
